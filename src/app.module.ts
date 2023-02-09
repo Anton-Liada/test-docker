89df013c11c5840db11cf0961a74759a8c94a17c
Anton Liada <liadaantom@gmail.com>
2023-02-09 17:12:20 +0200

    4

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { ProfileModule } from './profile/profile.module';
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
+      // port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
