f811bad2506a283238c7d4acf3085900e001acc3
Anton Liada <liadaantom@gmail.com>
2023-02-09 17:52:36 +0200

    5

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import bodyParser from 'body-parser';
 
 async function start() {
   const PORT = process.env.PORT || 3333;
   const app = await NestFactory.create(AppModule, { cors: true });
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(bodyParser.json());
 
   await app.listen(PORT);
 }
